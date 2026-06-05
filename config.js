971b7fb5411daaaf507f9628973fb45e7565fe59
htay-htay-thwe <130959683+htay-htay-thwe@users.noreply.github.com>
2026-06-05 21:07:42 +0700

    main

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const JWT_SECRET = process.env.JWT_SECRET || "dev-secret";
 const NODE_ENV = process.env.NODE_ENV || "development";
